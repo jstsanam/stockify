ba91f350762bda7212e3a35ba11c32663e3e5204
Sanam Yadav <jstsanam@gmail.com>
2025-02-22 07:55:42 +0530

    Added logic for notifications using websockets

--- src/components/stock-detail/notifications/Notifications.tsx
+++ src/components/stock-detail/notifications/Notifications.tsx
@@ -1,12 +1,55 @@
+import { useEffect, useState } from "react";
 import "./Notifications.scss";
+import { getSocket } from "../../../utils/socket";
 
-export default function Notifications() {
+interface NotificationsType {
+  currentStock: any;
+}
+
+export default function Notifications({ currentStock }: NotificationsType) {
+  const socket = getSocket();
+
+  const [notifications, setNotifications] = useState<any[]>([]);
+
+  useEffect(() => {
+    socket.on("transactionUpdate", (data: any) => {
+      setNotifications((prevNotifications) => [...prevNotifications, data]);
+    });
+
+    return () => {
+      socket.off("transactionUpdate");
+    };
+  }, []);
+
+  useEffect(() => {
+    if (currentStock?._id) {
+      setNotifications([]);
+    }
+  }, [currentStock?._id]);
+
   return (
     <div className="notifications-box">
       <div className="notifications-title">NOTIFICATIONS 🔔</div>
-      <div style={{ marginLeft: "0.5rem", color: "#a2a2a2" }}>
-        No notifications available for this stock.
-      </div>
+      {notifications.length === 0 ? (
+        <div style={{ marginLeft: "0.5rem", color: "#a2a2a2" }}>
+          No notifications available for this stock.
+        </div>
+      ) : (
+        <div className="notifications">
+          {notifications.map((notification, index) => (
+            <div key={index} className="notification-card">
+              <div>{notification.message}</div>
+              <div>
+                {new Date(notification.time).toLocaleTimeString("en-US", {
+                  hour: "2-digit",
+                  minute: "2-digit",
+                  hour12: true,
+                })}
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
